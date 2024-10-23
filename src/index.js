da089663ee8c84e8742e57470c5a3fbfca998b9f
topDevelopers123 <parasjisco@gmail.com>
2024-10-23 16:57:10 +0530

    origin change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4100;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({ origin: `http://localhost:3000`, credentials: true }));
+app.use(cors({ origin: `https://cmpb123.netlify.app`, credentials: true }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
