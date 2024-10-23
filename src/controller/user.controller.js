f1eb50d565642c7ae7731d882372b3aa848ba1ea
topDevelopers123 <parasjisco@gmail.com>
2024-10-23 17:32:30 +0530

    check

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -143,7 +143,7 @@ const loginUser = async (req, res) => {
   // Setting cookies with various configurations
 
   // 1. Cookie: 'lax'
-  res.cookie("lax", "lax  httpOnly: false secure: true", {
+  res.cookie("A-lax", "lax  httpOnly: false secure: true", {
     httpOnly: false,
     secure: true,
     maxAge: 3600000, // 1 hour
@@ -152,7 +152,7 @@ const loginUser = async (req, res) => {
   });
 
   // 2. Cookie: 'empty-1.1'
-  res.cookie("empty-1.1", "with path  httpOnly: false secure: true", {
+  res.cookie("A-empty-1.1", "with path  httpOnly: false secure: true", {
     httpOnly: false,
     secure: true,
     maxAge: 3600000, // 1 hour
@@ -160,7 +160,7 @@ const loginUser = async (req, res) => {
   });
 
   // 3. Cookie: 'empty-1.2'
-  res.cookie("empty-1.2", "without path  httpOnly: false secure: true", {
+  res.cookie("A-empty-1.2", "without path  httpOnly: false secure: true", {
     httpOnly: false,
     secure: true,
     maxAge: 3600000, // 1 hour
@@ -168,7 +168,7 @@ const loginUser = async (req, res) => {
   });
 
   // 4. Cookie: 'strict'
-  res.cookie("strict", " httpOnly: false secure: true", {
+  res.cookie("A-strict", " httpOnly: false secure: true", {
     httpOnly: false,
     secure: true,
     maxAge: 3600000, // 1 hour
@@ -179,7 +179,7 @@ const loginUser = async (req, res) => {
   // Part 2
 
   // 5. Cookie: 'lax-2'
-  res.cookie("lax-2", "lax  httpOnly: true secure: false", {
+  res.cookie("A-lax-2", "lax  httpOnly: true secure: false", {
     httpOnly: true,
     secure: false,
     maxAge: 3600000, // 1 hour
@@ -188,7 +188,7 @@ const loginUser = async (req, res) => {
   });
 
   // 6. Cookie: 'strict-2'
-  res.cookie("strict-2", " httpOnly: true secure: false", {
+  res.cookie("A-strict-2", " httpOnly: true secure: false", {
     httpOnly: true,
     secure: false,
     maxAge: 3600000, // 1 hour
