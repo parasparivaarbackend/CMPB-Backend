dd8a442b1992e7cb0949ce494d7a28772c8cfb7f
topDevelopers123 <parasjisco@gmail.com>
2024-10-23 16:13:16 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4100;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({ origin: `${process.env.ORIGIN}`, credentials: true }));
+app.use(cors({ origin: `http://localhost:3000`, credentials: true }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
