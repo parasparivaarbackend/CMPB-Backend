c60fcf4c0cdd3a399f0e3c33c4a554c1f9e621fe
topDeveloper123 <jhahimanshu498@gmail.com>
2024-10-23 15:02:12 +0530

    change registered controller

--- src/controller/Profile/presentaddress.controller.js
+++ src/controller/Profile/presentaddress.controller.js
@@ -1,31 +1,53 @@
-import {z} from 'zod'
+import { z } from 'zod'
 import { presentaddressmodels } from '../../model/Profile/PresentAddress.js';
 
 const validateSchema = z.object({
-    ProfileID:z.string().min(2),
-    Country:z.string().min(2),
-    State:z.string().min(2),
-    City:z.string().min(2),
-    Pincode:z.string().min(2)
+    Country: z.string().min(2),
+    State: z.string().min(2),
+    City: z.string().min(2),
+    Pincode: z.string().min(2)
 })
 
-const CreatePresentAddress =async(req, res)=>{
+const CreatePresentAddress = async (req, res) => {
     const data = req.body;
+    const { ProfileID } = req.params
     const validateData = validateSchema.safeParse(data);
 
     if (validateData.success === false) {
         return res.status(400).json({ ...validateData.error.issues });
     }
 
-    const insertData = await presentaddressmodels.create(validateData.data)
+    const insertData = await presentaddressmodels.create({ ProfileID, ...validateData.data })
     res.status(200).json({
-        message:"Present Address create successfull",
-        data:insertData
+        message: "Present Address create successfull",
+        data: insertData
     })
-    
+
+}
+
+const UpdatePresentAddress = async (req, res) => {
+    const { id } = req.params
+    const data = req.body
+    const userId = req.user._id
+
+    // const existUser = await 
+ 
+    const validateData = validateSchema.safeParse(data);
+
+    if (validateData.success === false) {
+        return res.status(400).json({ ...validateData.error.issues });
+    }
+    try {
+
+        const updateData = await presentaddressmodels.findByIdAndUpdate(id, { ...validateData.data }, { new: true })
+        console.log(updateData);
+        return res.status(200).json(updateData);
+    } catch (error) {
+        console.log(error);
+
+
+    }
 }
 
 
-
-
-export { CreatePresentAddress }
+export { CreatePresentAddress, UpdatePresentAddress }
--- src/routes/Profile/PresentAddress.routes.js
+++ src/routes/Profile/PresentAddress.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { CreatePresentAddress } from "../../controller/Profile/presentaddress.controller.js";
+import { CreatePresentAddress, UpdatePresentAddress } from "../../controller/Profile/presentaddress.controller.js";
 import asyncHandler from "../../utils/asyncHandler.js";
 import AuthMiddleware from "../../middleware/Auth.middleware.js";
 
 
 const presentaddressRouter = Router()
 
-presentaddressRouter.route("/create").post(AuthMiddleware, asyncHandler(CreatePresentAddress))
+presentaddressRouter.route("/create/:ProfileID").post(AuthMiddleware, asyncHandler(CreatePresentAddress))
+presentaddressRouter.route("/update/:ProfileID/:id").post(AuthMiddleware, asyncHandler(UpdatePresentAddress))
 
 export default presentaddressRouter
