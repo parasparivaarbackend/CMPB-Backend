ba0f367f0a2baf94091e1fde3f61e3ad36a6905b
topDevelopers123 <parasjisco@gmail.com>
2024-10-23 12:57:30 +0530

    httpOnly: false, secure: false, will change later

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -30,12 +30,12 @@ const registeredUser = async (req, res) => {
   const token = GenerateToken(data._id);
   res
     .cookie("token", token, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
     })
     .cookie("role", data.role, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
     });
   return res.status(StatusCodes.OK).json({
     message: "User registered successfull",
