c32f124d903718c226ddb769449014d391e54cb8
topDevelopers123 <parasjisco@gmail.com>
2024-10-24 10:48:22 +0530

    dawdwa

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4100;
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: `https://celebrated-daffodil-6ac963.netlify.app`,
+    origin: `*`,
     credentials: true,
   })
 );
