ad305326e9a160754d144ef33e0ad7905298f580
topDevelopers123 <parasjisco@gmail.com>
2024-10-24 18:57:05 +0530

    multer and cloud bucket

--- src/middleware/multter.middleware.js
+++ src/middleware/multter.middleware.js
@@ -2,29 +2,49 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp/images");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, "CMPB-" + Date.now() + "-" + file.originalname);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const fileType = /jpeg|jpg|png|webp/;
-  const extName = fileType.test(file.originalname.toLowercase());
-  const mimetype = fileType.test(file.mimetype);
-  if (extName && mimetype) {
+const ImagefileFilter = (req, file, cb) => {
+  const fileTypes = /jpeg|jpg|png|webp/;
+  const extname = fileTypes.test(file.originalname.toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+  if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb(new Error(`Error in uploading file`));
+    cb(new Error(`only images are allowed ${fileTypes}`));
   }
 };
 
 export const uploadImage = multer({
   storage: storage,
-  fileFilter: fileFilter,
+  fileFilter: ImagefileFilter,
   limits: {
     files: 3,
-    fieldSize: 500 * 1024,
+    fileSize: 512 * 1024,
+  },
+});
+
+const VideofileFilter = (req, file, cb) => {
+  const fileTypes = /mp4|mov|avi|mkv|flv|wmv/;
+  const extname = fileTypes.test(file.originalname.toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error(`only Videos are allowed ${fileTypes}`));
+  }
+};
+
+export const uploadVideo = multer({
+  storage: storage,
+  fileFilter: VideofileFilter,
+  limits: {
+    files: 1,
+    fileSize: 50 * 1024 * 1024,
   },
 });
---
+++ src/utils/CloudBucketHandler.js
@@ -0,0 +1,46 @@
+import { Storage } from "@google-cloud/storage";
+import fs from "fs";
+const storage = new Storage({
+  keyFilename: "keys.json",
+});
+
+export async function UploadBucketHandler(file, dest) {
+  try {
+    const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+    const destination = `CMPB/${dest}/${file.filename}`;
+
+    const data = await bucket.upload(file.path, {
+      destination: destination,
+      resumable: true,
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    fs.unlinkSync(file.path);
+    const URL = `https://storage.googleapis.com/${data[1].bucket}/${data[1].name}`;
+    return { URL, uploadID: data[1].name };
+  } catch (error) {
+    fs.unlinkSync(file.path);
+    console.log(error);
+  }
+}
+export async function DeleteBucketFile(id) {
+  try {
+    const file = storage.bucket(process.env.BUCKET_NAME).file(id);
+    const [exists] = await file.exists();
+
+    if (!exists) {
+      console.log("File does not exist");
+      return `File does not exist`;
+    }
+
+    await file.delete();
+    return `Deleted Successfully`;
+  } catch (error) {
+    console.log("Failed to Delete from Bucket");
+    console.log("Failed to Delete File from Bucket ", error);
+    throw new Error(`Failed to Delete File from Bucket `);
+  }
+}
