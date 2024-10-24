0e25c24e07630d3f0d83b39e229d5fd7d09170ca
topDevelopers123 <parasjisco@gmail.com>
2024-10-24 11:32:35 +0530

    origin

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4100;
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: `*`,
+    origin: `https://celebrated-daffodil-6ac963.netlify.app`,
     credentials: true,
   })
 );
