bbeb7dc3b44524ac362895bc33b7c8f3b987aa54
topDeveloper123 <jhahimanshu498@gmail.com>
2024-10-29 15:49:35 +0530

    event setup

--- src/controller/eventDetails/eventDetails.contoller.js
+++ src/controller/eventDetails/eventDetails.contoller.js
@@ -1,11 +1,26 @@
+import { z } from "zod";
 import { eventdetails } from "../../model/Events/eventdetails.model.js";
 
 
+const eventsSchema = z.object({
+    availableDates:z.string().min(2),
+    venues:z.string().min(2),
+    amount:z.string().min(2),
+    eventName:z.string().min(2)
+})
+
 const CreateEventDetails = async(req, res)=>{
-    const ProfileID = req.admin.ProfileID.toString();
     const createData = req.body;
-    console.log(ProfileID);
-    console.log(createData);   
+    
+    const validationData = eventsSchema.safeParse(createData)
+    if (validationData.success === false) {
+        return res.status(400).json({...validationData.error.issues})
+    }
+
+    const data = await eventdetails.create({...validationData.data})
+
+    return res.status(200).json({message:"Events Details Created Succesfull", data })
+    
 }
 
 export { CreateEventDetails }
