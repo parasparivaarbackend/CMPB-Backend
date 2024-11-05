93fa94e349c7a3881de9cc93ab7317c4ca201e0a
topDeveloper123 <jhahimanshu498@gmail.com>
2024-11-05 13:51:51 +0530

    deactivate

--- src/controller/Deactivate/deactivate.controller.js
+++ src/controller/Deactivate/deactivate.controller.js
@@ -1,18 +1,17 @@
 import { UserModel } from "../../model/user.model.js"
 
-const DeactivateAccount = async(req, res)=>{
-    const {id} = req.params;
-    
+const DeactivateAccount = async (req, res) => {
+    const id = req.user?._id;
     try {
-        await UserModel.findByIdAndUpdate(id, {active:false})
+        await UserModel.findByIdAndUpdate(id, { active: false })
         res.clearCookie("token").clearCookie("role")
-        return res.status(200).json({message:"Account Deactivate Succesfull"})
+        return res.status(200).json({ message: "Account Deactivate Succesfull" })
     } catch (error) {
         console.log(error);
-        
+
     }
-    
-    
+
+
 }
 
 export { DeactivateAccount }
