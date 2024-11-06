22555499b5f22f06aa8863cfb81e832561930410
topDevelopers123 <parasjisco@gmail.com>
2024-11-06 16:11:27 +0530

    ...

--- src/routes/ChurayeHuaPal/ChurayeHuaPal.routes.js
+++ src/routes/ChurayeHuaPal/ChurayeHuaPal.routes.js
@@ -1,16 +1,30 @@
 import { Router } from "express";
-import { ChurayeHuaPalCreate, ChurayeHuaPalDelete, ChurayeHuaPalGet, ChurayeHuaPalUpdate } from "../../controller/ChurayeHuaPal/ChurayeHuaPal.controller.js";
-import { AdminAuthMiddleware } from "../../middleware/Auth.middleware.js"
+import {
+  ChurayeHuaPalCreate,
+  ChurayeHuaPalDelete,
+  ChurayeHuaPalGet,
+  ChurayeHuaPalUpdate,
+} from "../../controller/ChurayeHuaPal/ChurayeHuaPal.controller.js";
+import { AdminAuthMiddleware } from "../../middleware/Auth.middleware.js";
 import asyncHandler from "../../utils/asyncHandler.js";
 
 const ChurayeHuaPalRouter = Router();
 
-ChurayeHuaPalRouter.route("/get").get(AdminAuthMiddleware, asyncHandler(ChurayeHuaPalGet))
+ChurayeHuaPalRouter.route("/get").get(asyncHandler(ChurayeHuaPalGet));
 
-ChurayeHuaPalRouter.route("/create").post(AdminAuthMiddleware, asyncHandler(ChurayeHuaPalCreate))
+ChurayeHuaPalRouter.route("/create").post(
+  AdminAuthMiddleware,
+  asyncHandler(ChurayeHuaPalCreate)
+);
 
-ChurayeHuaPalRouter.route("/update/:id").put(AdminAuthMiddleware, asyncHandler(ChurayeHuaPalUpdate))
+ChurayeHuaPalRouter.route("/update/:id").put(
+  AdminAuthMiddleware,
+  asyncHandler(ChurayeHuaPalUpdate)
+);
 
-ChurayeHuaPalRouter.route("/delete/:id").delete(AdminAuthMiddleware, asyncHandler(ChurayeHuaPalDelete))
+ChurayeHuaPalRouter.route("/delete/:id").delete(
+  AdminAuthMiddleware,
+  asyncHandler(ChurayeHuaPalDelete)
+);
 
-export { ChurayeHuaPalRouter }
+export { ChurayeHuaPalRouter };
