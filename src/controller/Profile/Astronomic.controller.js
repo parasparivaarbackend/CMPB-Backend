75ef0d09d714a1c7a611464c2a141588fd5a9988
topDevelopers123 <parasjisco@gmail.com>
2024-11-14 15:55:07 +0530

    PartnerExpectationModel changes

--- src/controller/Profile/Astronomic.controller.js
+++ src/controller/Profile/Astronomic.controller.js
@@ -2,51 +2,59 @@ import { z } from "zod";
 import { AstronomicModel } from "../../model/Profile/Astronomic.model.js";
 
 const AstronomicSchema = z.object({
-    SunSign: z.string().min(2),
-    MoonSign: z.string().min(2),
-    TimeOfBirth: z.string().min(2),
-    CityOfBirth: z.string().min(2)
-})
+  SunSign: z.string().min(2),
+  MoonSign: z.string().min(2),
+  TimeOfBirth: z.string().min(2),
+  CityOfBirth: z.string().min(2),
+});
 
-const CreateAstronomic = async(req, res)=>{
-    const ProfileID = req.user.ProfileID.toString();
-    const createData = req.body;
+const CreateAstronomic = async (req, res) => {
+  const ProfileID = req.user.ProfileID.toString();
+  const createData = req.body;
 
-    const validateData = AstronomicSchema.safeParse(createData)
-    if (validateData.success === false) {
-        return res.status(400).json({...validateData.error.issues})
-    }
+  const validateData = AstronomicSchema.safeParse(createData);
+  if (validateData.success === false) {
+    return res.status(400).json({ ...validateData.error.issues });
+  }
 
-    const checkAstronomic = await AstronomicModel.findOne({ ProfileID })
-    if (checkAstronomic) {
-        return res.status(400).json({message:"Astronomic Details already exist"})
-    }
+  const checkAstronomic = await AstronomicModel.findOne({ ProfileID });
+  if (checkAstronomic) {
+    return res
+      .status(400)
+      .json({ message: "Astronomic Details already exist" });
+  }
 
-    const data = await AstronomicModel.create({ProfileID, ...validateData.data})
-    return res.status(200).json({ message: "Astronomic Details Created Succesfull", data })
-    
-}
+  const data = await AstronomicModel.create({
+    ProfileID,
+    ...validateData.data,
+  });
+  return res
+    .status(200)
+    .json({ message: "Astronomic Details Created Succesfull", data });
+};
 
+const UpdateAstronomic = async (req, res) => {
+  const ProfileID = req.user.ProfileID.toString();
+  const updateData = req.body;
 
-const UpdateAstronomic = async(req, res)=>{
-    const ProfileID = req.user.ProfileID.toString();
-    const updateData = req.body;
-   
-    const validateData = AstronomicSchema.safeParse(updateData)
-    if (validateData.success === false) {
-        return res.status(400).json({...validateData.error.issues})
-    }
+  const validateData = AstronomicSchema.safeParse(updateData);
+  if (validateData.success === false) {
+    return res.status(400).json({ ...validateData.error.issues });
+  }
 
-   try {
-     const data = await AstronomicModel.findOneAndUpdate({ProfileID}, {ProfileID, ...validateData.data}, {new:true})
- 
-     return res.status(200).json({message:"Astronomic Details Created Succesfull", data })
-   } catch (error) {
+  try {
+    const data = await AstronomicModel.findOneAndUpdate(
+      { ProfileID },
+      { ProfileID, ...validateData.data },
+      { new: true }
+    );
+
+    return res
+      .status(200)
+      .json({ message: "Astronomic Details Created Succesfull", data });
+  } catch (error) {
     console.log(error);
-    
-   }
-    
-}
+  }
+};
 
-
-export { CreateAstronomic, UpdateAstronomic }
+export { CreateAstronomic, UpdateAstronomic };
--- src/model/Profile/PartnerExpectation.model.js
+++ src/model/Profile/PartnerExpectation.model.js
@@ -1,119 +1,120 @@
 import mongoose, { Schema } from "mongoose";
 
 const partnerExpectationSchema = new mongoose.Schema({
-    ProfileID: {
-        type: Schema.Types.ObjectId,
-        ref: "profiles",
-    }, 
-    GernalRequirement: {
-        type: String,
-        require: true,
-        trim: true
-    },
+  ProfileID: {
+    type: Schema.Types.ObjectId,
+    ref: "profiles",
+  },
+  GernalRequirement: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 
-    ResidenceCountry: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Height: {
-        type: Number,
-        require: true,
-        trim: true
-    },
+  ResidenceCountry: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Height: {
+    type: Number,
+    require: true,
+    trim: true,
+  },
 
-    weight: {
-        type: Number,
-        require: true,
-        trim: true
-    },
-    MaritalStatus: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Children: {
-        type: Boolean,
-        require: true,
-        trim: true
-    },
-    Religion: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Caste: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    SubCaste: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Language: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Education: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    Profession: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    SmokingAcceptable: {
-        type: Boolean,
-        require: true,
-        trim: true
-    },
-    DietAcceptable: {
-        type: Boolean,
-        require: true,
-        trim: true
-    },
-    DrinkAcceptable: {
-        type: Boolean,
-        require: true,
-        trim: true
-    },
-    personalValue: {
-        type: Number,
-        require: true,
-        trim: true
-    },
-    Manglik: {
-        type: Boolean,
-        require: true,
-        trim: true
-    },
-    PreferredCountry: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    PreferredState: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    FamilyValue: {
-        type: Number,
-        require: true,
-        trim: true
-    },
-    Complexion: {
-        type: String,
-        require: true,
-        trim: true
-    }
-
-
+  weight: {
+    type: Number,
+    require: true,
+    trim: true,
+  },
+  MaritalStatus: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Children: {
+    type: Boolean,
+    require: true,
+    trim: true,
+  },
+  Religion: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Caste: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  SubCaste: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Language: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Education: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  Profession: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  SmokingAcceptable: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  DietAcceptable: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  DrinkAcceptable: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  personalValue: {
+    type: Number,
+    require: true,
+    trim: true,
+  },
+  Manglik: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  PreferredCountry: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  PreferredState: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  FamilyValue: {
+    type: Number,
+    require: true,
+    trim: true,
+  },
+  Complexion: {
+    type: String,
+    require: true,
+    trim: true,
+  },
 });
 
-export const PartnerExpectationModel = mongoose.model("partnerexpectations", partnerExpectationSchema);
+export const PartnerExpectationModel = mongoose.model(
+  "partnerexpectations",
+  partnerExpectationSchema
+);
