ba8963ccb68c2f4e53797769c2203533e291cd44
topDevelopers123 <parasjisco@gmail.com>
2024-12-02 10:47:10 +0530

    more attributes added in profile models

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ const PORT = process.env.PORT || 4100;
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: [
-      `http://localhost:3000`,
-      // `https://celebrated-daffodil-6ac963.netlify.app`,
-      "https://api.chatmangnipatbyah.org/api/v1",
-    ],
+    origin: [`http://localhost:3000`, "https://api.chatmangnipatbyah.org"],
     credentials: true,
   })
 );
--- src/model/Profile/Career.model.js
+++ src/model/Profile/Career.model.js
@@ -1,30 +1,55 @@
 import mongoose, { Schema } from "mongoose";
 
 const CareerSchema = new mongoose.Schema({
-    ProfileID: {
-        type: Schema.Types.ObjectId,
-        ref: "profiles",
-    },
+  ProfileID: {
+    type: Schema.Types.ObjectId,
+    ref: "profiles",
+    required: true,
+  },
+  currentJob: {
     designation: {
-        type: String,
-        required:true,
-        trim:true
+      type: String,
+      required: true,
+      trim: true,
     },
     company: {
-        type: String,
-        required:true,
-        trim:true
+      type: String,
+      required: true,
+      trim: true,
     },
     start: {
-        type: String,
-        required:true,
-        trim:true
+      type: String,
+      required: true,
+      trim: true,
     },
     end: {
-        type: String,
-        required:true,
-        trim:true
+      type: String,
+      required: false,
+      trim: true,
     },
+  },
+  previousJobs: {
+    designation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    company: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    start: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    end: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
 });
 
 export const careermodel = mongoose.model("careers", CareerSchema);
